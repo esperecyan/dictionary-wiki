f88466d48c78226ed73c901cbd12ffb73546caf5
100の人 <esperecyan@users.noreply.github.com>
2017-01-12 10:30:00 +0900

    トップページにサイトの説明を記載

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,36 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">{{ _('辞書まとめwiki') }}</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <p>
+                        {{ _('次のゲームで使う辞書のアップローダーです。') }}
+                    </p>
+                    <ul>
+                        <li><a href="http://www.forest.impress.co.jp/library/software/catchfeeling/" rel="external" target="_blank">
+                            {{ _('キャッチフィーリング') }}
+                        </a></li>
+                        <li><a href="http://drafly.nazo.cc/games/olds/DC" rel="external" target="_blank">
+                            {{ _('Drawing Catch') }}
+                        </a></li>
+                        <li><a href="http://vodka-catchm.seesaa.net/article/115922159.html" rel="external" target="_blank">
+                            {{ _('きゃっちま') }}
+                        </a></li>
+                        <li><a href="http://loxee.web.fc2.com/inteli.html" rel="external" target="_blank">
+                            {{ _('Inteligenceω') }}
+                        </a></li>
+                        <li><a href="http://pictsense.com/" rel="external" target="_blank">
+                            {{ _('ピクトセンス') }}
+                        </a></li>
+                    </ul>
+                    <p>
+                        {{ _('アップロードした辞書は、あとから皆で更新していくことができます。このアップローダーには辞書の相互変換機能があるので、たとえばピクトセンスの辞書を登録したら、キャッチフィーリングやInteligenceωのしりとり辞書としても利用できます。') }}
+                    </p>
+                    <p>
+                        {{ _('アップローダーの各コンテンツへは、ページ上部のメニューからどうぞ。') }}
+                        <strong>{{ _('ウィンドウの幅が狭いときは折りたたまれます。') }}</strong>
+                    </p>
                 </div>
             </div>
         </div>
